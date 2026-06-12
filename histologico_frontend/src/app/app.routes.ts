1e2337081a8ce3541920595f5376d036bed5a20e
Itsukar <hiraga.chevalier@gmail.com>
2026-06-11 23:29:28 -0500

    EDA terminado

--- histologico_frontend/src/app/app.routes.ts
+++ histologico_frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import DashboardLayout from './layouts/DashboardLayout/DashboardLayout';
 import DashboardPage from './pages/dashboard-page/dashboard-page';
 import { PrediccionPage } from './pages/prediccion-page/prediccion-page';
 import { MetricasPage } from './pages/metricas-page/metricas-page';
+import { EdaPage } from './pages/eda-page/eda-page';
 
 export const routes: Routes = [
   {
@@ -20,6 +21,10 @@ export const routes: Routes = [
       {
         path: 'metricas',
         component: MetricasPage
+      },
+      {
+        path: 'eda',
+        component: EdaPage
       },
       {
         path: '**',
---
+++ histologico_frontend/src/app/pages/eda-page/eda-page.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eda-page',
+  imports: [],
+  templateUrl: './eda-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EdaPage {}
